771fafbcbddac22a04bb8a0028d9a0a2249c79f8
alinck <linck.alex@gmail.com>
2016-10-30 20:42:43 -0400

    setup for angular complete authentication setup

--- authentication/models.py
+++ authentication/models.py
@@ -29,6 +29,7 @@ class AccountManager(BaseUserManager):
 
         return account
 
+
 class Account(AbstractBaseUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=40, unique=True)
--- mpcomm/urls.py
+++ mpcomm/urls.py
@@ -17,7 +17,20 @@ from django.conf.urls import url
 from django.contrib import admin
 from radiodb import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from rest_framework_nested import routers
 
+from authentication.views import AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
+urlpatterns = patterns(
+     '',
+    # ... URLs
+    url(r'^api/v1/', include(router.urls)),
+
+    url('^.*$', IndexView.as_view(), name='index'),
+)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
